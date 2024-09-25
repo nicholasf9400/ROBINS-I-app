6d272b22a0de50eca902c814dbc1bcd3ef5d09d2
Nicholas Fitzhugh <nicholas@fitzhugh.dk>
2024-09-25 14:20:57 +0200

    Initial commit

---
+++ ui.R
@@ -0,0 +1,51 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+navbarPage('Test app',
+           tabPanel('Domæne 1',
+                    id = 'dom1',
+                    'test1',
+                    fluidPage(
+                      fluidrow(
+                        column()
+                      )
+                    )
+                    
+                    
+                    ),
+           tabPanel('Domæne 2',
+                    id = 'dom2',
+                    'test1'
+           ),
+           tabPanel('Domæne 3',
+                    id = 'dom3',
+                    'test1'
+           ),
+           tabPanel('Domæne 4',
+                    id = 'dom4',
+                    'test1'
+           ),
+           tabPanel('Domæne 5',
+                    id = 'dom5',
+                    'test1'
+           ),
+           tabPanel('Domæne 6',
+                    id = 'dom6',
+                    'test1'
+           ),
+           tabPanel('Domæne 7',
+                    id = 'dom7',
+                    'test1'
+           )
+           
+           
+)
